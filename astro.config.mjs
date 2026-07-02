fbbefee1c05915408f8e2514acd86b7798dcdd3a
Hang Hua <huahang3202456@gmail.com>
2026-07-03 02:07:51 +0800

    fix mobile responsive layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-    site: "https://arifmolla13446.github.io",
-
-    // 如果你的仓库名不是 arifmolla13446.github.io，
-    // 而是 ai-demo-portfolio，就改成这样：
-    // base: "/ai-demo-portfolio",
+    site: "https://h3202456.github.io",
 });
